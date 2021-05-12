c2a0cfd07da6a5b05b23aa46bc088d602fd49a3e
Greifelt228 <lavrenchukvitaliy70@gmail.com>
2021-05-12 20:43:23 +0300

    70%

--- main.py
+++ main.py
@@ -1,21 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///music_group.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
 
+class Albums(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    # date = db.Column(db.DateTime, default=datetime.utcnow())
 
+    def __repr__(self):
+        return '<Albums %r>' % self.id
 @app.route('/')
-def index():
-    return 'Hello Worlddd!'
+@app.route('/home')
+def home():
+    return render_template('homePage.html')
 
 
 @app.route('/about')
 def about():
-    return 'About project!'
+    return render_template('aboutPage.html')
 
 
 @app.route('/history')
 def history():
-    return 'History grouppp'
+    return render_template('historyPage.html')
+
+
+@app.route('/albums')
+def albums():
+    albums = Albums.query.order_by(Albums.title.desc())
+    return render_template('albumsPage.html', albums=albums)
+
+
+@app.route('/albums/<int:id>')
+def albums_detail(id):
+    albums = Albums.query.get(id)
+    return render_template('albumDetail.html', albums=albums)
+
+
+@app.route('/add-albums', methods=['POST', 'GET'])
+def add_albums():
+    if request.method =='POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        album = Albums(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(album)
+            db.session.commit()
+            return redirect('/albums')
+        except:
+            return 'Помилка при додаванні'
+
+    else:
+        return render_template('addAlbumsPage.html')
 
 
 if __name__ == '__main__':
